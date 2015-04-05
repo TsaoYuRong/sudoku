935a6b80c98ef935dfa1e71f9ba0956508dc774d
TsaoYuRong <tammylinca@gmail.com>
2015-04-05 12:14:07 +0800

    sudoku.h

---
+++ sudoku.h
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class sudoku{
+public:
+	sudoku();
+class sudoku{
+public:
+	sudoku();
+	bool isCorrect();
+	void GiveQuestion();
+	void ReadIn(int[]);
+	void Solve();
+	static const int sudokuSize=144;
+
+private:
+	bool checkUnity(int arr[]);
+	int map[sudokuSize];
+	
+
+
+
+}
