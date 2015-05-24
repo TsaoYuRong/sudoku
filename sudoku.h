07a3a1df64e124332771ee0b1601f2c92c6693f4
TsaoYuRong <tammylinca@gmail.com>
2015-05-24 22:49:06 +0800

    sudoku.h

--- sudoku.h
+++ sudoku.h
@@ -5,10 +5,12 @@ public:
 	sudoku();
 	bool isCorrect(int []);
 	void GiveQuestion();
-	void ReadIn();
-	void Solve();
-	static const int sudokuSize=144;
+	sudoku ReadIn();
+	bool Solve(sudoku);
 	bool checkUnity(int arr[]);
+	int getFirstZero(int temAnswer[]);
+	void setElement(int,int);
+	static const int sudokuSize=144;
 
 private:
 	int map[sudokuSize];
